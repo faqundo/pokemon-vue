719e771f841cc3ceadb3e123d0223d47157efa3e
faqundo <facundo.ferro@werden.com.ar>
2021-07-14 23:54:30 -0300

    install vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
+import Vue from 'vue'/* 
 import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css'
+import 'vuesax/dist/vuesax.css' */
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(Vuesax, {
-  // options here
+/* Vue.use(Vuesax, {
+  
 })
-
+ */
 new Vue({
   router,
   render: h => h(App)
