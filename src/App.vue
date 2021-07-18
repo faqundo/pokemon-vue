f1cc80d8d47d63d9f389817eef77470021f3d27d
faqundo <facundo.ferro@werden.com.ar>
2021-07-18 19:32:25 -0300

    delete developer nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <v-app>
-    <router-link to="/">Home</router-link>
-    <router-link to="/loading">Loading</router-link>
-    <router-link to="/search">Search</router-link>
     <v-main>
       <router-view/>
     </v-main>
